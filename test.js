b6ede67d3278911af2d1511f31f5074d843ddc5a
raziquehasan <164611217+raziquehasan@users.noreply.github.com>
2025-06-14 11:34:22 +0530

    Create test.js

---
+++ test.js
@@ -0,0 +1,18 @@
+<html>
+    <head>
+
+    </head>
+    <body>
+        <form>
+            <fieldset style="width:40%;">
+                <h1 id="t1">Replace me</h1>
+                <input type="button" value="click me" onclick="disp(1)">
+                <input type="button" value="Wish Me" onclick="disp(2)">
+            </fieldset>
+        </form>
+        <script src="test.js"></script>
+           
+            
+        
+    </body>
+</html>
